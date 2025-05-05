f7cfbdc55508c226791369a18dc1188ae87a838b
DLK624 <165545205+DLK624@users.noreply.github.com>
2025-05-05 13:09:21 +0530

    gg

--- rest-countries-app/src/App.jsx
+++ rest-countries-app/src/App.jsx
@@ -65,21 +65,6 @@ function App() {
       </header>
 
       <main className="">
-        {/* {user && (
-          <div className="mb-6 bg-green-50 border border-green-200 text-green-700 px-4 py-3 rounded-md shadow-sm">
-            <div className="flex">
-              <div className="py-1">
-                <svg className="h-5 w-5 text-green-500" fill="currentColor" viewBox="0 0 20 20">
-                  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
-                </svg>
-              </div>
-              <div className="ml-3">
-                <p className="text-sm font-medium">Logged in as {user.email}</p>
-              </div>
-            </div>
-          </div>
-        )} */}
-
         <div className="bg- shadow-sm rounded-lg ">
           <Routes>
             <Route path="/" element={<CountryList />} />
--- rest-countries-app/src/utils/axiosConfig.js
+++ rest-countries-app/src/utils/axiosConfig.js
@@ -1,4 +1,3 @@
-// src/utils/axiosConfig.js
 import axios from 'axios';
 
 axios.defaults.withCredentials = true;
